d01df8f27890d8909319818765d7d7704c7e2134
alexssandro772 <alex.araujo77@gmail.com>
2023-02-03 16:56:08 -0300

    ULTIMAS ALTERAÇÕES DO DIA

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -2,6 +2,12 @@ class TiposVariaveis {
     
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao Java com Github!");
+        System.out.println("Tipos de variáveis: ");
+        System.out.println("Tipos primitivos: Int, float, double, long, boolean, char.");
+
+        System.out.println("FIM");
+    
+    
     }
     
 }
