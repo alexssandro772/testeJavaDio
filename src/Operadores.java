61fb02a898c3019cf5e3e5b0a8ee99ea3ac642b1
alexssandro772 <alex.araujo77@gmail.com>
2023-02-03 19:16:24 -0300

    operadores e seus tipos

---
+++ src/Operadores.java
@@ -0,0 +1,60 @@
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) {
+        System.out.println("Operadores");
+
+//     //classe Operadores.java
+//     String nome = "Alexssandro";
+//     int idade = 34;
+//     double peso = 72.5;
+//     char sexo = 'M';
+//     boolean doadorOrgao = false;
+//     Date dataNascimento = new Date();
+//     // System.out.println(dataNascimento);
+
+//     //classe Operadores.java
+// String nomeCompleto = "LINGUAGEM" + "JAVA";
+		
+// //qual o resultado das expressoes abaixo?
+// String concatenacao ="?"; 
+// // System.out.println(concatenacao);
+// concatenacao = 1+1+1+"1";
+// System.out.println(concatenacao);
+// concatenacao = 1+"1"+1+1;
+// System.out.println(concatenacao);
+// concatenacao = 1+"1"+1+"1";
+// System.out.println(concatenacao);
+// concatenacao = "1"+1+1+1;
+// System.out.println(concatenacao);
+// concatenacao = "1"+(1+1+1);
+// System.out.println(concatenacao);	
+    
+
+//classe Operadores.java
+int numero = 5;
+		
+//Imprimindo o numero negativo
+System.out.println(- numero);
+
+//incrementando numero em mais 1 numero, imprime 6
+numero ++;
+System.out.println(numero);
+
+//incrementando numero em mais 1 numero, imprime 7
+System.out.println(numero ++);// ops algo de errado não está certo
+
+System.out.println(numero);// agora sim, numero virou 7
+
+//ordem de precedencia conta aqui
+System.out.println(++ numero);
+
+boolean verdadeiro = true;
+
+System.out.println("Virou " + !verdadeiro);
+
+
+    
+
+    }
+}
