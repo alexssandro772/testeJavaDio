121b1517e1ab80f534e784714fc57af45576a106
alexssandro772 <alex.araujo77@gmail.com>
2023-02-03 21:16:17 -0300

    Trabalhando com Operadores Lógicos

--- src/Operadores.java
+++ src/Operadores.java
@@ -53,24 +53,56 @@ public class Operadores {
 
 // System.out.println("Virou " + !verdadeiro);
 
-System.out.println("Operador ternário");
-// classe Operadores.java
-int a, b;
+// System.out.println("Operador ternário");
+// // classe Operadores.java
+// int a, b;
 
-a = 5;
-b = 6;
+// a = 5;
+// b = 6;
 
-/*
-if(a==b)
-   resultado = "verdadeiro";
-else
-   resultado = "falso";
-*/
+// /*
+// if(a==b)
+//    resultado = "verdadeiro";
+// else
+//    resultado = "falso";
+// */
 
-String resultado = (a==b) ? "verdadeiro" : "false";
+// String resultado = (a==b) ? "verdadeiro" : "false";
 
-System.out.println(resultado);
+// System.out.println(resultado);
 
+
+
+// System.out.println("Trabalhando com Comparações");
+// //classe Operadores.java
+// int numero1 = 1;
+// int numero2 = 2;
+// if (numero1 == numero2) {
+//     System.out.println("Numero 1 é igual ao número 2");
+// }
+
+// if(numero1 > numero2)
+// 	System.out.println("Numero 1 maior que numero 2");
+
+// if(numero1 < numero2)
+// 	System.out.println("Numero 1 menor que numero 2");
+
+// if(numero1 >= numero2)
+// 	System.out.println("Numero 1 maior ou igual que numero 2");
+
+// if(numero1 <= numero2)
+// 	System.out.println("Numero 1 menor ou igual que numero 2");
+
+// if(numero1 != numero2)
+// 	System.out.println("Numero 1 é diferente de numero 2");
+System.out.println("Trabalhando com Operadores Lógicos");
+
+boolean condicao1 = false;
+boolean condicao2 = true;
+
+if (condicao1 == false && condicao2 == true) {
+    System.out.println("No se");
+}
 
     
 
